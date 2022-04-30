680d211de6de5b758879faf04c7b75d7ee894206
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2022-04-30 23:56:02 +0200

    renaming changesets numbers; adding trace llergens

---
+++ backend/recipes/src/main/java/com/heartcooking/tracesallergen/TracesAllergenRepository.java
@@ -0,0 +1,6 @@
+package com.heartcooking.tracesallergen;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TracesAllergenRepository extends JpaRepository<TracesAllergen, Long> {
+}
